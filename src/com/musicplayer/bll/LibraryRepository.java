52d268f61e75e4ac7bae0e6ab4fecd8dffc3dce6
Terry Rockfield <trock5150@embarqmail.com>
2012-12-09 13:16:02 -0500

    Few more minor changes to UserRepo

--- src/com/musicplayer/bll/LibraryRepository.java
+++ src/com/musicplayer/bll/LibraryRepository.java
@@ -4,18 +4,25 @@
  */
 package com.musicplayer.bll;
 
+import TitanPlayer.util.HibernateUtil;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 
 /**
  *
  * @author Kyle
  */
 public class LibraryRepository {
-    List<Library> libraries;
+    private ArrayList<Library> libraries = new ArrayList<Library>();
     
     public LibraryRepository() {
-        libraries = new ArrayList<Library>();
+        libraries = loadLibrary();
     }
     
     public void addLibrary(Library libraryToAdd) {
@@ -37,5 +44,85 @@ public class LibraryRepository {
         userLibrary.addUser(user);
         libraries.add(userLibrary);
         return userLibrary;
+    }
+
+    private ArrayList<Library> loadLibrary() {
+        String libUsersListQuery = "select distinct lib.userid from LibraryDBRecord lib";
+        
+        libraries = getAllSongsHQLQuery(libUsersListQuery);
+        
+        return libraries;
+    }
+
+    private ArrayList<Library> getAllSongsHQLQuery(String libUsersListQuery) {
+        UserRepository userList = new UserRepository();
+        Library currentLibrary = new Library();
+        ArrayList<Library> allLibraries = new ArrayList<Library>();
+        
+        String querySong = "select lib.songName from LibraryDBRecord lib";
+        String queryArtist = "select lib.songArtist from LibraryDBRecord lib";
+        String queryPath = "select lib.songPath from LibraryDBRecord lib";
+        String subQuery1 = " where lib.userid = '";
+        String subQuery2 = " and lib.songName = '";
+        
+        List nameList = null;
+        List songList = null;
+        List artistList = null;
+        List pathList = null;
+        
+        UserAccount currentUser = null;
+        
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+                org.hibernate.Query qN = session.createQuery(libUsersListQuery);       
+                nameList = qN.list();
+                
+                for(int i = 0; i < nameList.size(); i++){
+                    currentLibrary = new Library();
+                    String currentName = (String) nameList.get(i);
+
+                    try {
+                        currentUser = userList.getUser(currentName);
+                    } catch (Exception ex) {
+                        Logger.getLogger(LibraryRepository.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    
+                    org.hibernate.Query qS = session.createQuery(querySong + subQuery1 + nameList.get(i)+"'");       
+                    songList = qS.list();
+
+                    for(int s = 0; s < songList.size(); s++){
+                        org.hibernate.Query qA = session.createQuery(queryArtist + subQuery1 + nameList.get(i) + "'" + subQuery2 + songList.get(s) + "'");       
+                        artistList = qA.list();
+                        
+                        org.hibernate.Query qP = session.createQuery(queryPath + subQuery1 + nameList.get(i) + "'" + subQuery2 + songList.get(s) + "'");       
+                        pathList = qP.list();
+                        
+                        String libSongName = (String) songList.get(s);
+                        String libSongArtist = (String) artistList.get(0);
+                        String libSongPath = (String) pathList.get(0);
+                        Path libSong = Paths.get(libSongPath);
+                        
+                        Song addToLib = new Song(libSongName, libSongArtist, libSong);
+
+                        try {
+                            currentLibrary.addSong(addToLib);
+                        } catch (Exception ex) {
+                            Logger.getLogger(LibraryRepository.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+
+                    }
+                    currentLibrary.addUser(currentUser);
+                    allLibraries.add(currentLibrary);
+                    
+                }
+                
+                session.getTransaction().commit();
+
+        } catch (HibernateException he) {
+            he.printStackTrace();
+        }
+
+        return allLibraries;
     }
 }
