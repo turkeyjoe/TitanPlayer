0bb062eb171f147569cd722703f4aa1fe418da8e
Terry Rockfield <trock5150@embarqmail.com>
2012-12-09 09:13:14 -0500

    Had to add additional getter and setter methods

--- src/com/musicplayer/bll/UserAccount.java
+++ src/com/musicplayer/bll/UserAccount.java
@@ -44,6 +44,32 @@ public class UserAccount {
         return email;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    
+    
     private boolean validatePassword(String pass) {
         Pattern p = Pattern.compile("\\d+");
         Matcher m = p.matcher(pass);
